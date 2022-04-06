1d75db1b70c7c9de15b74c14a4be8a7163802ee7
jyko <yeon0125@gmail.com>
2022-04-07 00:34:48 +0900

    prettier, textInput color

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,10 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: true,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'all',
-  printWidth: 80,
-  tabWidth: 4,
+    arrowParens: 'avoid',
+    bracketSameLine: true,
+    bracketSpacing: true,
+    jsxBracketSameLine: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    printWidth: 80,
+    tabWidth: 4,
 };
